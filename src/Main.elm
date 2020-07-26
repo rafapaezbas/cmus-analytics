4e143e80e7c0d1714dd9a3936a37437081412935
rpaezbas <rafaelpaezbastida@gmail.com>
2020-07-26 12:43:03 +0200

    removed duplicated methods

--- src/Main.elm
+++ src/Main.elm
@@ -26,28 +26,6 @@ main =
 type alias Sample = { artist: String, album: String, title: String, year: String}
 type alias Model = { samples : List Sample }
 
-decodeSample: String -> Result Json.Decode.Error Sample
-decodeSample sample =
-    Json.Decode.decodeString (
-                 Json.Decode.map4
-                 Sample
-                 (field "artist" string)
-                 (field "album" string)
-                 (field "title" string)
-                 (field "year" string)
-                 ) sample
-
-extractSample: Result Json.Decode.Error Sample -> Sample
-extractSample result =
-    case result of
-        Err _ ->
-            { artist = "default", album = "default" , title = "default" , year = "default" }
-        Ok sample ->
-            sample
-
-isNotDefaultSample: Sample -> Bool
-isNotDefaultSample sample = .artist sample /= "default" || .album sample /= "default" || .title sample /= "default" || .year sample /= "default"
-
 init : String -> ( Model, Cmd Msg )
 init fileData =
     let
