03c28b9e45416a72889fd8aa3ecc89827d151463
rpaezbas <rafaelpaezbastida@gmail.com>
2020-08-06 00:00:58 +0200

    added save and get by id

---
+++ docs/index.js
@@ -0,0 +1,82 @@
+var log;
+
+window.onload = () => {
+
+	if(checkIdParam()){
+		cleanUpDescription();
+		var id = getIdParam();
+		getLog(id).then(initElm);
+
+	}
+
+	document.getElementById('save').addEventListener('click',function (){
+		saveChart(log).then(console.log);
+	});
+
+	document.getElementById('import').addEventListener('click',function (){
+		openFileDialog();
+	});
+
+	document.getElementById('file').addEventListener('change', function() {
+		cleanUpDescription();
+		createChart(this);
+	});
+
+};
+
+
+var checkIdParam = () => {
+	var urlParams = new URLSearchParams(window.location.search);
+	return urlParams.has('id');
+};
+
+var getIdParam = () => {
+	var urlParams = new URLSearchParams(window.location.search);
+	return urlParams.get('id');
+};
+
+var openFileDialog = () => {
+	document.getElementById('file').click();
+};
+
+var createChart = (e) => {
+	var fr=new FileReader();
+	fr.onload=function(){
+		log = fr.result; // Side effect!!
+		initElm(fr.result);
+	}
+	fr.readAsText(e.files[0]);
+};
+
+var saveChart = (log) => {
+    const headers = {
+      Accept: "*/*",
+      "Content-Type": "text/plain",
+    };
+    return fetch("http://3.9.171.210/logs", {
+      method: "POST",
+      headers: headers,
+      body: log,
+    }).then((res) => res.text());
+};
+
+var cleanUpDescription = () => {
+	document.getElementById('description').remove();
+};
+
+var getLog = (id) => {
+    const headers = {
+      Accept: "text/plain, */*",
+    };
+    return fetch("http://3.9.171.210/logs/" + id, {
+      method: "GET",
+      headers: headers,
+    }).then((res) => res.text());
+};
+
+var initElm = (model) => {
+	var app = Elm.Main.init({
+		node: document.getElementById('myapp'),
+		flags: model
+	});
+};
